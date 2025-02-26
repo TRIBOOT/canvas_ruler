e824f42bb80f9c0664f33e94557eaa5f24b723cc
jaho <jannik.hoelper@triboot.de>
2025-02-26 12:13:22 +0100

    Updated in dev part. Run npm serve to create dist

--- app/js/rulerConstructor.js
+++ app/js/rulerConstructor.js
@@ -44,38 +44,68 @@ ruler.prototype.rulerConstructor =  function(_canvas, options, rulDimension)
 
 
     var drawPoints = function () {
-        var  pointLength = 0,
-            label = '',
-            delta = 0,
-            draw = false,
-            lineLengthMax = 0,
-            lineLengthMed = rulThickness / 2,
-            lineLengthMin = rulThickness / 2;
+      var pointLength = 0,
+        label = '',
+        delta = 0,
+        draw = false,
+        lineLengthMax = 0,
+        lineLengthMed = rulThickness / 2,
+        lineLengthMin = rulThickness / 2;
 
-        for (var pos = 0; pos <= rulLength; pos += 1) {
-            delta = ((rulLength / 2) - pos);
-            draw = false;
-            label = '';
+      // do ten steps between each mark
+      // then calculate how much space and value each step needs
+      // then get the maximum number of points
+      var distanceBetweenLabeledMarks = 50;
+      var stepsInBetween = 10;
+      var diffPerMark = distanceBetweenLabeledMarks / stepsInBetween;
+      var positionDeltaPerStep = (distanceBetweenLabeledMarks / rulScale) / stepsInBetween;
+      var maxPoints = (rulLength /positionDeltaPerStep);
 
-            if (delta % 50 === 0) {
-                pointLength = lineLengthMax;
-                label = Math.round(Math.abs(delta)*rulScale);
-                draw = true;
-            }
-            else if (delta % 25 === 0) {
-                pointLength = lineLengthMed;
-                draw = true;
-            }
-            else if (delta % 5 === 0) {
-                pointLength = lineLengthMin;
-                draw = true;
-            }
-            if (draw) {
-                context.moveTo(pos + 0.5, rulThickness + 0.5);
-                context.lineTo(pos + 0.5, pointLength +  0.5);
-                context.fillText(label, pos + 1.5, (rulThickness / 2) + 1);
-            }
+      // to always have 0 / 0 at the top left of the image, calculate the offset error made by rounding
+      // this will shift the ruler according to the error made by rounding
+      var middlePointIndexOffset = (maxPoints - Math.floor(maxPoints)) / 2;
+
+      //now calculate the position offset on the ruler
+      var middlePointPositionOffset = middlePointIndexOffset * positionDeltaPerStep;
+
+      // get the overall offset to set 0 into the middle of the canvas
+      delta = ((rulLength * rulScale) / 2);
+
+      // since we want to have our marks in certain steps (here : 5), we shift the values back to the next 5
+      // steps like 2->7->12 would be steps of 5 too, but we want to have 0->5->10 etc.
+      var valueOffset = delta % diffPerMark;
+
+      // if the maxPoints is not an even number, we get another offset
+      if(Math.floor(maxPoints/2) !== Math.round(maxPoints/2)) {
+        var missingSteps = (maxPoints/2) - Math.floor(maxPoints/2);
+        middlePointPositionOffset = missingSteps * positionDeltaPerStep;
+      }
+
+      // only do as many steps a needed
+      for(var pos = 0;pos <=maxPoints; pos += 1){
+        var value = Math.round(delta - (pos * diffPerMark) - valueOffset) * -1;
+
+        // do big marks every distanceBetweenLabeledMarks steps
+        if(value % distanceBetweenLabeledMarks === 0){
+          pointLength = lineLengthMax;
+          label =  value;
+          draw = true;
         }
+        else
+        {
+          // do small mark
+          pointLength = lineLengthMed;
+          draw = true;
+          label = '';
+        }
+
+        //draw
+        if(draw) {
+          context.moveTo(pos * positionDeltaPerStep + middlePointPositionOffset, rulThickness + 0.5);
+          context.lineTo(pos * positionDeltaPerStep+ middlePointPositionOffset, pointLength + 0.5);
+          context.fillText(label, (pos * positionDeltaPerStep) + 1.5 + middlePointPositionOffset, (rulThickness / 2) + 1);
+        }
+      }
     };
 
     var mousemove = function(e) {
